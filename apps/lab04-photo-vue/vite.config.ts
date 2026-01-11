6952321e12c944835703392123c46de588571895
Meg007x <bowonnun.t@kkumail.com>
2026-01-11 20:38:35 +0700

    Lab4-2

--- apps/lab04-photo-vue/vite.config.ts
+++ apps/lab04-photo-vue/vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Moblie_phone/',  // ชื่อ repo ของคุณบน GitHub
+    base: './',  // ชื่อ repo ของคุณบน GitHub
   plugins: [
     vue(),
     legacy()
